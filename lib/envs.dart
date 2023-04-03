29f572fcbaddc73233d308bdf0af53b28dd7717f
nuno314 <tinle1234vt@gmail.com>
2023-04-03 23:52:11 +0700

    add dependencies

---
+++ lib/envs.dart
@@ -0,0 +1,13 @@
+class Env {
+  static const appName = 'appname';
+  static const baseApiLayer = 'baseApiLayer';
+
+  static const devEnvName = 'Development';
+  static const stagingEnvName = 'Staging';
+  static const prodEnvName = 'Production';
+
+  static final Map<String, dynamic> dev = {
+    appName: 'FBS',
+    baseApiLayer: '',
+  };
+}
